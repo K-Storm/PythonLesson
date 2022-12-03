6abdd2d1c70b0e22e5e7ff5fe8520acc822dc208
HIROFUMI <touroku319@gmail.com>
2022-12-03 17:55:08 +0900

    test

---
+++ hello.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import ttk
+
+def sub_window(i):
+    def test():
+        sub_root = Tk()
+        sub_root.title('S3 ls')
+        sub_frame = ttk.Frame(sub_root, padding=16)
+        sub_label = ttk.Label(sub_frame, text='S3バケット検索')
+        print(i)
+    return test()
+
+root = Tk()
+root.title('AWS S3 CRUD')
+
+frame1 = ttk.Frame(root, padding=16)
+label1 = ttk.Label(frame1, text='Your name')
+t = StringVar()
+entry1 = ttk.Entry(frame1, textvariable=t)
+button1 = ttk.Button(
+    frame1,
+    text='OK',
+    command=sub_window('ff')
+)
+
+frame1.pack()
+label1.pack(side=LEFT)
+entry1.pack(side=LEFT)
+button1.pack(side=LEFT)
+
+root.mainloop()
