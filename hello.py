472a383ac1851e92e01f1a23eb19bdc71156f21f
HIROFUMI <touroku319@gmail.com>
2022-12-10 22:12:15 +0900

    s3にオブジェクトを作成する処理を追加

--- hello.py
+++ hello.py
@@ -1,28 +1,48 @@
 from tkinter import *
 from tkinter import ttk
+import boto3
+import os
+
+AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
+
+def s3_ls():
+    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    bucket_name = "***"
+    directory_name = "test/test2"
+    s3.put_object(Bucket=bucket_name, Key=(directory_name+'/'))
+
 
 def sub_window():
     sub_root = Tk()
-    sub_root.title('S3 ls')
+    sub_root.title('aws s3 CRUD')
     sub_frame = ttk.Frame(sub_root, padding=16)
     sub_label = ttk.Label(sub_frame, text='S3バケット検索')
+    str = StringVar()
+    sub_entry = ttk.Entry(sub_frame, textvariable=str)
+    sub_button = ttk.Button(
+        sub_frame,
+        text='PutObject',
+        command=lambda: s3_ls()
+    )
+
+    sub_frame.pack()
+    sub_label.pack(side=LEFT)
+    sub_entry.pack(side=LEFT)
+    sub_button.pack(side=LEFT)
 
 root = Tk()
 root.title('AWS S3 CRUD')
 
 frame1 = ttk.Frame(root, padding=16)
-label1 = ttk.Label(frame1, text='Your name')
 t = StringVar()
-entry1 = ttk.Entry(frame1, textvariable=t)
 button1 = ttk.Button(
     frame1,
-    text='OK',
+    text='START',
     command=lambda: sub_window()
 )
 
 frame1.pack()
-label1.pack(side=LEFT)
-entry1.pack(side=LEFT)
 button1.pack(side=LEFT)
 
 root.mainloop()
