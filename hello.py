f228342edf109a4ba07b92ef71b1024d704f5717
HIROFUMI <touroku319@gmail.com>
2022-12-10 22:44:15 +0900

    バケットを一覧取得する処理を実装

--- hello.py
+++ hello.py
@@ -5,31 +5,47 @@ import os
 
 AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-def s3_ls():
-    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+def s3_put_object():
     bucket_name = "***"
     directory_name = "test/test2"
     s3.put_object(Bucket=bucket_name, Key=(directory_name+'/'))
+
+def s3_get_object():
+    res = s3.list_buckets()
+    print(res)
 
 
 def sub_window():
     sub_root = Tk()
     sub_root.title('aws s3 CRUD')
     sub_frame = ttk.Frame(sub_root, padding=16)
-    sub_label = ttk.Label(sub_frame, text='S3バケット検索')
+    sub_label = ttk.Label(sub_frame, text='S3オブジェクト更新')
     str = StringVar()
     sub_entry = ttk.Entry(sub_frame, textvariable=str)
     sub_button = ttk.Button(
         sub_frame,
         text='PutObject',
-        command=lambda: s3_ls()
+        command=lambda: s3_put_object()
+    )
+
+    get_label = ttk.Label(sub_frame, text='S3バケット検索')
+    str = StringVar()
+    get_entry = ttk.Entry(sub_frame, textvariable=str)
+    get_button = ttk.Button(
+        sub_frame,
+        text='GetObject',
+        command=lambda: s3_get_object()
     )
 
     sub_frame.pack()
     sub_label.pack(side=LEFT)
     sub_entry.pack(side=LEFT)
     sub_button.pack(side=LEFT)
+    get_label.pack(side=LEFT)
+    get_entry.pack(side=LEFT)
+    get_button.pack(side=LEFT)
 
 root = Tk()
 root.title('AWS S3 CRUD')
