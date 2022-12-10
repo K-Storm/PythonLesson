7e27dea563278a726c4b508c32320f345f2bf98d
HIROFUMI <touroku319@gmail.com>
2022-12-10 23:13:03 +0900

    s3オブジェクトの削除処理実装

--- hello.py
+++ hello.py
@@ -7,15 +7,22 @@ AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
 s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
+# オブジェクト更新
 def s3_put_object():
-    bucket_name = "***"
-    directory_name = "test/test2"
-    s3.put_object(Bucket=bucket_name, Key=(directory_name+'/'))
+    bucket_name = "kunimitsu-test"
+    directory_name = "test/test4"
+    s3.put_object(Bucket=bucket_name, Key=directory_name+'/')
 
+# バケット一覧取得
 def s3_get_object():
     res = s3.list_buckets()
     print(res)
 
+# バケットのオブジェクト削除
+def s3_delete_object():
+    bucket_name = "kunimitsu-test"
+    directory_name = "test/test4"
+    s3.delete_object(Bucket=bucket_name, Key=directory_name+'/')
 
 def sub_window():
     sub_root = Tk()
@@ -39,6 +46,15 @@ def sub_window():
         command=lambda: s3_get_object()
     )
 
+    delete_label = ttk.Label(sub_frame, text='S3バケット検索')
+    str = StringVar()
+    delete_entry = ttk.Entry(sub_frame, textvariable=str)
+    delete_button = ttk.Button(
+        sub_frame,
+        text='DeleteObject',
+        command=lambda: s3_delete_object()
+    )
+
     sub_frame.pack()
     sub_label.pack(side=LEFT)
     sub_entry.pack(side=LEFT)
@@ -46,6 +62,9 @@ def sub_window():
     get_label.pack(side=LEFT)
     get_entry.pack(side=LEFT)
     get_button.pack(side=LEFT)
+    delete_label.pack(side=LEFT)
+    delete_entry.pack(side=LEFT)
+    delete_button.pack(side=LEFT)
 
 root = Tk()
 root.title('AWS S3 CRUD')
