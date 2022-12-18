b8eb66ed181bf2838bee0bbdee056f0811bb22f3
HIROFUMI <touroku319@gmail.com>
2022-12-18 15:51:29 +0900

    profileを用いるように修正

--- mys3cruds.py
+++ mys3cruds.py
@@ -3,9 +3,8 @@ from tkinter import ttk
 import boto3
 import os
 
-AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
-s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+session = boto3.Session(profile_name="k-storm")
+s3= session.client('s3')
 
 class S3Methods:
     # オブジェクト更新
